0bb1531829b3d79a27dad555db607c77cd424c97
Alexandr-Shestihin <70847183+Alexandr-Shestihin@users.noreply.github.com>
2021-10-27 22:33:23 +0300

    Updates

---
+++ static/js/6.202f569d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-project"]=this["webpackJsonpmy-project"]||[]).push([[6],{260:function(t,c,e){},268:function(t,c,e){"use strict";e.r(c);e(0),e(260);var n=e(1);c.default=function(){return Object(n.jsx)("div",{children:"News"})}}}]);
+//# sourceMappingURL=6.202f569d.chunk.js.map
